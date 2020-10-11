6927eae8f2c6cb7460ad9b8feb3615e60d260313
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2020-10-11 20:31:12 +0300

    symlink for both powershell and powershell core

--- posh/install.ps1
+++ posh/install.ps1
@@ -1,12 +1,21 @@
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
-Install-Module posh-git -Scope CurrentUser
-Install-Module oh-my-posh -Scope CurrentUser
+$POWERSHELL_DIR = Join-Path -Path $env:USERPROFILE -ChildPath "Documents\WindowsPowerShell"
+# symlink for powershell and powershell core using one profile
+New-Item `
+    -Path (Join-Path -Path $env:USERPROFILE -ChildPath "Documents\PowerShell") `
+    -ItemType SymbolicLink `
+    -Value $POWERSHELL_DIR `
+    -Force
 
 # symlink to powershell profile
 New-Item `
-    -Path $PROFILE `
+    -Path (Join-Path -Path $POWERSHELL_DIR -ChildPath "Microsoft.PowerShell_profile.ps1") `
     -ItemType SymbolicLink `
     -Value (
         Join-Path -Path $PSScriptRoot -ChildPath "profile.ps1" `
         ).ToString() `
     -Force
+
+
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
